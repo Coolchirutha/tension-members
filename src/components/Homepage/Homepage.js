e4dc76065780e6b2a19844c537bb23f792eaa8bc
Mouni Nischal <d.chiranjeevi007@gmail.com>
2021-03-16 05:32:51 +0530

    initial commit

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+import MemberCard from "../MemberCard/MemberCard";
+import classes from "./Homepage.module.css";
+
+// Image Imports
+import SingleBolted from "../../images/single_angle_section_with_bolted_connections.png";
+import TwoSame from "../../images/two_angle_sections_placed_back_to_back_on_same_side_of_gusset_plate.png";
+import TwoOpposite from "../../images/two_angle_sections_placed_back_to_back_on_opposite_sides_of_gusset_plate.png";
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className={classes.container}>
+        <div className={classes.header}>Design of Tension Members</div>
+        <div className={classes.content}>
+          <div className={classes.subheader}>
+            Choose the type of member you want to design
+          </div>
+          <div className={classes.cardList}>
+            <MemberCard
+              image={SingleBolted}
+              altText="Single Angle Section with Bolted Connections"
+              className={classes.left}
+            ></MemberCard>
+            <MemberCard
+              image={SingleBolted}
+              altText="Single Angle Section with Bolted Connections"
+              className={classes.right}
+            ></MemberCard>
+            <MemberCard
+              altText="Two Angle Sections placed back to back on same side of Gusset Plate"
+              image={TwoSame}
+              className={classes.left}
+            ></MemberCard>
+            <MemberCard
+              image={TwoOpposite}
+              altText="Two Angle Sections placed back to back on opposite side of Gusset Plate"
+              className={classes.right}
+            ></MemberCard>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
---
+++ src/components/MemberCard/MemberCard.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import classes from "./MemberCard.module.css";
+
+export default class MemberCard extends Component {
+  constructor(props){
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className={classes.container}>
+        <img
+          src={this.props.image}
+          alt={this.props.altText}
+        />
+      </div>
+    );
+  }
+}
